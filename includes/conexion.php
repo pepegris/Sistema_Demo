f1492ba13750522c157fa7e77a8122d1ef548f2c
Andres Salcedo <44298815+pepegris@users.noreply.github.com>
2021-10-16 12:09:47 -0400

    up

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,7 +1,7 @@
 <?php
 
-/* $conn_string = "host=ec2-34-202-54-225.compute-1.amazonaws.com port=5432 dbname=d7n0493g0lg7gc user=txzlwvlnufptva password=18a749721241a5aa82818a7c77158c437b05238e9964224ac4615b469bd2ffd5"; */
-$conn_string = "host=localhost port=5432 dbname=demo user=postgres password=753159*";
+ $conn_string = "host=ec2-23-22-243-103.compute-1.amazonaws.com port=5432 dbname=dd4i4cbbkpbd7v user=iiunqbagsrcnfo password=64ee48171b92117938b6851213ee1d9d781903fe3ae09bad5439b5970b48d1d0 "; 
+/* $conn_string = "host=localhost port=5432 dbname=demo user=postgres password=753159*"; */
 $conn = pg_connect($conn_string);
 
 
